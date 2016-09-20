e57839b6e55d8c270912dd9bb1799b18aa6c573c
GitMAGI <leonardo.paglionico@gmail.com>
2016-09-21 00:56:14 +0200

    Coomit Finale oggi

--- RISPrivati/Batch/Program.cs
+++ RISPrivati/Batch/Program.cs
@@ -18,6 +18,8 @@ namespace Batch
             Stopwatch tw = new Stopwatch();
             tw.Start();
 
+            string provenienza = "RISPrivati";
+
             log.Info("Starting procedure...");
 
             DataAccessLayer.DAL dal = new DataAccessLayer.DAL();
@@ -30,7 +32,7 @@ namespace Batch
                 //2. Lettura Vista Gro -- select * from [eurosanita].[VW_EsamiRadiologiciRISCasilino] -- where IDRichiesta non è preso in carico
 
                 //3. Verifica Anagrafica Paziente
-                List<RISCentral> rissC = new List<RISCentral>();
+                List<RISCentral> rissC = new List<RISCentral>(); // Fittizio
                 foreach (RISCentral risC in rissC)
                 {
                     string name = risC.Nome;
@@ -39,9 +41,50 @@ namespace Batch
                     DateTime? birth = risC.DataNascita;
                     if(name!=null && surname!=null && birth!=null && cf!= null){
                         Pazi paz = dal.GetPazienteByFilter(name, surname, cf, birth.Value);
-                        if (paz != null)
+                        if (paz == null)
                         {
                             //4. In caso non esista, creare il pazeinte in GR
+                            Pazi nuovoPaziente = new Pazi();
+                            nuovoPaziente.PAZINOME = risC.Nome;
+                            nuovoPaziente.PAZICOGN = risC.Cognome;
+                            nuovoPaziente.PAZICOFI = risC.CodiceFiscale;
+                            nuovoPaziente.PAZIDATA = risC.DataNascita;
+                            int written = dal.InsertPaziente(nuovoPaziente);
+                            if (written != 1)
+                            {
+                                //ERROR! FATAL
+                                continue;
+                            }
+
+                            //5. Creare il mappning in IdentityMapping
+                            IdentityMapping nuovoMapping = new IdentityMapping();
+                            nuovoMapping.idext = risC.IdPaziente.ToString();
+                            nuovoMapping.provenienza = provenienza;
+                            written = dal.InsertIdentityMapping(nuovoMapping);
+                            if (written != 1)
+                            {
+                                //ERROR! FATAL
+                                continue;
+                            }
+                        }
+                        else
+                        {
+                            // L'anagrafica esiste in GR. Bisogna controllare se esite in IdentintyMapping, se no bisogna crearla
+                            int IDPazienteCentral = risC.IdPaziente.Value;
+                            int IDPazienteGR = paz.PAZIIDID.Value;
+                            IdentityMapping mapping = dal.GetIdentityMappingByPK(IDPazienteGR, IDPazienteCentral.ToString());
+                            if(mapping == null)
+                            {
+                                IdentityMapping nuovoMapping = new IdentityMapping();
+                                nuovoMapping.idext = IDPazienteCentral.ToString();
+                                nuovoMapping.provenienza = provenienza;
+                                int written = dal.InsertIdentityMapping(nuovoMapping);
+                                if(written != 1)
+                                {
+                                    //ERROR! FATAL
+                                    continue;
+                                }
+                            }
                         }
                     }
 
--- RISPrivati/Batch/dal/IdentityMappingDAO.cs
+++ RISPrivati/Batch/dal/IdentityMappingDAO.cs
@@ -1,6 +1,7 @@
 ﻿using Batch.vo;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,6 +19,44 @@ namespace DataAccessLayer
             affetcedRows = DBSQL.InsertOperation(GRConnectionString, tabName, data);
 
             return affetcedRows;
+        }
+
+        public IdentityMapping GetIdentityMappingByPK(int idGR, string idCentral)
+        {
+            IdentityMapping data = null;
+
+            string tabName = "IdentityMapping";
+
+            Dictionary<string, DBSQL.QueryCondition> conditions = new Dictionary<string, DBSQL.QueryCondition>();
+            conditions.Add("idGR", new DBSQL.QueryCondition
+            {
+                Key = "idunico",
+                Op = DBSQL.Op.Equal,
+                Conj = DBSQL.Conj.And,
+                Value = idGR
+            });
+            conditions.Add("idCentral", new DBSQL.QueryCondition
+            {
+                Key = "idext",
+                Op = DBSQL.Op.Equal,
+                Conj = DBSQL.Conj.None,
+                Value = idCentral
+            });
+
+            DataTable data_ = DBSQL.SelectOperation(GRConnectionString, tabName, conditions);
+
+            log.Info(string.Format("Query Executed! Retrieved {0} records!", data_.Rows.Count));
+
+            if (data_ != null)
+            {
+                if (data_.Rows.Count == 1)
+                {
+                    data = IdentityMappingMapper.DataRowToVO(data_.Rows[0]);
+                    log.Info(string.Format("{0} Records mapped to {1}", 1, typeof(IdentityMapping).ToString()));
+                }
+            }
+
+            return data;
         }
     }
 }
